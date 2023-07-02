bbb8ec2605b5e4fe53a46ad391e3b864436d773b
Nikita Sakun <NikitaSakun1@gmail.com>
2023-07-02 20:42:10 +0300

    Complete Project

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Nikita on 12.06.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
+
+
